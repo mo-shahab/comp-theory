26d67e83cd5c3ff9bccfc3caee9499ea2dbd450f
= <mohdshahabuddinsoharwardi@gmail.com>
2022-02-12 23:19:24 +0530

    tis with the joins

--- dbms/index.sql
+++ dbms/index.sql
@@ -307,3 +307,36 @@ FROM CLIENT
 WHERE client_name LIKE '%school%';
 
 -- UNIONS
+-- find a list of employee and branch names
+
+SELECT first_name
+FROM employee
+UNION
+SELECT branch_name
+FROM branch
+UNION
+SELECT client_name
+FROM client;
+
+-- find a list of all clients & branch suppliers' names
+SELECT client_name, client.branch_id
+FROM client
+UNION
+SELECT supplier_name, branch_supplier.branch_id
+FROM branch_supplier;
+
+-- find a list of all money spent or earned by the company
+SELECT salary
+FROM employee
+UNION
+SELECT total_sales
+FROM works_with;
+
+-- JOINS
+INSERT INTO branch VALUES(4, 'Buffalo', NULL, NULL);
+
+-- find all branches and the names of their managers
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+JOIN branch
+ON employee.emp_id = branch.mgr_id; 
