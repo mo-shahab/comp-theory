d8a6ba2b0f9bdd6de6fdbee6fdd4833ec19982b6
= <mohdshahabuddinsoharwardi@gmail.com>
2022-02-20 00:13:08 +0530

    just need to discuss

--- quiz/quiz.c
+++ quiz/quiz.c
@@ -14,6 +14,9 @@ struct questions
 
 };
 
+int lives = 3;
+int score[10]= {0};
+
 int main(void)
 {
     struct questions questions;
@@ -23,9 +26,10 @@ int main(void)
     FILE *file = fopen(filename, "r");
     FILE *file1 = fopen(filename1, "r");
     FILE *file2 = fopen(filename2, "r");
-    int i, j = 0, k = 0, m, n = 0, f;
-    int x;
+    int i, j = 0, k = 0, m, n = 0, _randno, scrptr = 0;
+    int x, f;
     char choice, ch;
+    int temp, scorefinal, run = 1, consent;
 
 
     //file in the array done
@@ -79,19 +83,97 @@ int main(void)
 
     // this is for the access of the elements 
 
-    
-    for(k = 0, j = 0; k < i || j < m; k++, j++)
-    {
-        printf("%s\n", questions.ques[k]);
-        printf("%s\n", questions.options[j]);
-        printf("\n");
-    }
+    printf("--------------------------QUIZ--------------------------\n");
+    printf("=>Rules for the game are as given below.\n");
+    printf("=>Make sure you read them before you start\n");
+    printf("----------------------------------------------------------");
+    printf("\n->You have to answer 10 questions random questions\n");
+    printf("->They are mostly the MCQ type\n");
+    printf("->You first choose the correct answer and then input it in the console\n");
+    printf("->For every right answer you will be awarded points");
+    printf("->And you get only 3 chances, and they reduce each time you answer incorrectly\n");
+    printf("->Your score will be displayed at the end of the game\n");
+    printf("----------------------------------------------------------\n\n");
+    printf("Choose (y) to start playing the game or (n) to exit from the console\n");
+    scanf("%c", &choice);
 
-    for(f = 0; f < strlen(questions.answers); f++)
+    if(choice == 'y')
     {
-        printf("%c\t", questions.answers[f]);
-    }
+        while(run == 1)
+        {
+            x= 0;
+            
+            while(x < 10 && lives != 0)
+            {
+                f = rand() % strlen(questions.answers);
+                printf("\n=>%s\n", questions.ques[f]);
+                printf("%s\n\n", questions.options[f]);
+                scanf("%d", &temp);
 
+                if(questions.answers[f] - temp == 96)
+                {
+                    printf("great you answered correctly\n");
+                    score[scrptr] = 1;
+                    scrptr++;
+                }
+                else
+                {
+                    printf("Oh your answer is incorrect, unfortunately you lose a life\n");
+                    score[scrptr] = 0;
+                    scrptr++;
+                    lives--;
+                }
+                x++; 
+            }
+            if(lives == 0)
+            {
+                scorefinal = 0;
+                printf("'\nOh no you lost all the lives.\n");
+                printf("Your final score is: \n");
+                for(i = 0; i < 10; i++)
+                {
+                    if(score[i] == 1)
+                    {
+                        scorefinal = scorefinal + 5;
+                    }
+                }
+                printf("%d\n\n", scorefinal);
+            }
+            if(x == 10)
+            {
+                scorefinal = 0;
+                printf("You have finished all the 10 questions\n");
+                printf("Your Final score is: \n");
+                for(i = 0; i < 10; i++)
+                {
+                    if(score[i] == 1)
+                    {
+                        scorefinal = scorefinal + 5;
+                    }
+                }
+                printf("%d", scorefinal);
+            }
+            printf("\nDo you wanna continue playing\n");
+            printf("y/n\n");
+            scanf("%d", &consent);
+            if(consent == 1)
+            {
+                lives = 3;
+                run = 1;
+            }
+            else 
+            {
+                printf("Hope you come back and enjoy.");
+                run = 0;
+            } 
+            
+        }
+            
+    }
+    else
+    {
+        exit(1);
+    }
 
     return 0;
 }
