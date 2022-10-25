2522eee63dbdaac152a952f7ad6bb3fea2dd58c9
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2022-10-25 21:59:05 +0530

    basic bitch

---
+++ php/site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>something</title>
+</head>
+<body>
+    <h1>hewwo wowwd</h1>
+    <?php
+        echo("something something");
+        echo("<h1>something bigger</h1>");
+        echo("<hr>");
+        echo("<p style=\"font-family: Arial\">there are lot of things to work on and there shall be the work done so that there will be no lazing around and stuff so that
+        we can win whatever the thing that is brewing in the puny little delusional brains of ours and whatever that is will be slain by the 
+        chivalrous person who does not have anything to do with the thing and there shall be blood and stuff like that this is so cool");
+    ?>
+</body>
+</html>
